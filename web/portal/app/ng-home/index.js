37e91960f46e55804d5b2707ed5d26568e385a9b
Brandon Valosek <bvalosek@gmail.com>
2015-09-03 22:10:19 -0500

    web stuff

---
+++ web/portal/app/ng-home/index.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import htmlHome from './home.html';
+
+export default angular.module('centauri.home', [
+  uiRouter,
+])
+.config(($stateProvider) => {
+  $stateProvider.state('app.home', {
+    url: '/',
+    template: htmlHome,
+  });
+})
+
+;
+
