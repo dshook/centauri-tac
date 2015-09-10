1ac33be21d31552b71a0fc89fbe483acac87f0d6
Brandon Valosek <bvalosek@gmail.com>
2015-09-09 22:37:06 -0500

    adjsutment of auth

--- server/api/ComponentAPI.js
+++ server/api/ComponentAPI.js
@@ -7,8 +7,7 @@ import roles from '../middleware/roles.js';
 /**
  * REST API dealing with the Component model
  */
-@middleware(authToken())
-@middleware(roles(['component']))
+@middleware(authToken({ require: false }))
 export default class ComponentAPI
 {
   constructor(auth, components)
@@ -17,37 +16,35 @@ export default class ComponentAPI
     this.components = components;
   }
 
+  /**
+   * Master list of all components
+   */
   @route.get('/')
   async getAll()
   {
     return await this.components.all();
   }
 
+  /**
+   * Register a component with the master
+   */
   @route.post('/register')
+  @middleware(roles(['component']))
   async register(req)
   {
     const {url, name} = req.body;
     return await this.components.register(url, name);
   }
 
+  /**
+   * Ping component for keep-alive time
+   */
   @route.post('/ping/:id')
+  @middleware(roles(['component']))
   async ping(req)
   {
     const {id} = req.params;
     const component = await this.components.pingById(id);
-
-    if (!component) {
-      throw new HttpError(404, 'component not found');
-    }
-
-    return component;
-  }
-
-  @route.get('/:id')
-  async get(req)
-  {
-    const {id} = req.params;
-    const component = await this.components.getById(id);
 
     if (!component) {
       throw new HttpError(404, 'component not found');
