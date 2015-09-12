48d102ce403a7c6f3b87839994ad0296c98cfc4d
Brandon Valosek <bvalosek@gmail.com>
2015-09-12 14:38:53 -0500

    game stuff

--- server/api/GameAPI.js
+++ server/api/GameAPI.js
@@ -1,18 +1,39 @@
 import {route} from 'http-tools';
+import {middleware} from 'http-tools';
+import authToken from '../middleware/authToken.js';
+import roles from '../middleware/roles.js';
+import Game from 'models/Game';
 
 /**
  * REST API for dealing with Game models
  */
+@middleware(authToken())
 export default class GameAPI
 {
-  constructor(games)
+  constructor(games, auth)
   {
+    this.auth = auth;
     this.games = games;
   }
 
+  /**
+   * Big list of all games across realms, admin only
+   */
   @route.get('/')
+  // @middleware(roles(['admin']))
   async getAllGames()
   {
     return await this.games.all();
   }
+
+  /**
+   * Register a new game (RPC from game component)
+   */
+  @route.post('/register')
+  async registerGame(req)
+  {
+    const game = Game.fromJSON(req.body);
+    return await this.games.register(game);
+  }
+
 }
--- server/datastores/GameStore.js
+++ server/datastores/GameStore.js
@@ -2,6 +2,7 @@ import loglevel from 'loglevel-decorator';
 import Game from 'models/Game';
 import Component from 'models/Component';
 import ComponentType from 'models/ComponentType';
+import Player from 'models/Player';
 
 /**
  * Data layer for games
@@ -14,23 +15,52 @@ export default class GameStore
     this.sql = sql;
   }
 
+  /**
+   * Every damn game
+   */
   async all()
   {
-    const resp = await this.sql.tquery(Game, Component, ComponentType)(`
+    const resp = await this.sql.tquery(Game, Component, ComponentType, Player)(`
 
         select * from games as g
         join components c
           on g.game_component_id = c.id
         join component_types t
           on c.component_type_id = t.id
+        join players p
+          on g.host_player_id = p.id
 
       `, null,
-      (g, c, t) => {
+      (g, c, t, p) => {
         g.gameComponent = c;
         g.gameComponent.type = t;
+        g.hostPlayer = p;
         return g;
       });
 
     return resp.toArray();
+  }
+
+  /**
+   * Create a new game entry from a model, updates the id in the model
+   */
+  async register(game: Game): Promise<Game>
+  {
+    const resp = await this.sql.query(Game)(`
+
+      insert into games
+        (name, game_component_id, host_player_id)
+      values
+        (@name, @componentId, @hostPlayerId)
+      returning id
+
+      `,
+      game);
+
+    const {id} = resp.firstOrNull();
+
+    game.id = id;
+
+    return game;
   }
 }
