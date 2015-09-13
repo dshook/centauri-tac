2b6e047b6ebd7b4e0830eb31fb110c8db8446038
Brandon Valosek <bvalosek@gmail.com>
2015-09-13 17:27:33 -0500

    generic player migration for auth demo

---
+++ migrations/sqls/20150913222405-generic-players-down.sql
@@ -0,0 +1,3 @@
+delete from players where
+  email = 'player1@gmail.com'
+  or email = 'player2@gmail.com';
---
+++ migrations/sqls/20150913222405-generic-players-up.sql
@@ -0,0 +1,3 @@
+insert into players (email, password) values
+  ('player1@gmail.com', '$2a$10$aORNhtAgypxwmtBitiLd7.3AhG4F4EphkeblqzSCAwwnx3TRBNcom'),
+  ('player2@gmail.com', '$2a$10$DUQZT/zx.U1.vNDHn5gT6.3QKSr2DRTUOcRJJJRD4Fsy00hihnbgO');
