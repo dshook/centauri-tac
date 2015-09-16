28000e4c53b3dd8495253db46d6ce913cd3320ee
Dillon Shook <dshook@alumni.nmt.edu>
2015-09-15 23:08:35 -0400

    Terrible camera movement

---
+++ centauri-tac/Assets/Scripts/views/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+    bool dragging = false;
+
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            dragOrigin = Input.mousePosition;
+            dragging = true;
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonUp("Fire1"))
+        {
+            dragging = false;
+            return;
+        }
+
+        if (dragging)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * -dragSpeed, 0, pos.y * -dragSpeed);
+
+            transform.Translate(move, Space.World);
+        }
+    }
+
+
+}
