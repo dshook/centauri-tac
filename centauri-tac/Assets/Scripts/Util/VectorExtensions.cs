2c9d2ef4a7319b706ad1c376922042bc420aa414
Dillon Shook <dshook@alumni.nmt.edu>
2015-10-19 20:42:18 -0400

    Lame max card height

--- centauri-tac/Assets/Scripts/Util/VectorExtensions.cs
+++ centauri-tac/Assets/Scripts/Util/VectorExtensions.cs
@@ -23,5 +23,23 @@ namespace ctac
         {
             return new Vector2(vec.x, vec.z);
         }
+
+        public static Vector3 SetX(this Vector3 vec, float value)
+        {
+            vec.Set(value, vec.y, vec.z);
+            return vec;
+        }
+
+        public static Vector3 SetY(this Vector3 vec, float value)
+        {
+            vec.Set(vec.x, value, vec.z);
+            return vec;
+        }
+
+        public static Vector3 SetZ(this Vector3 vec, float value)
+        {
+            vec.Set(vec.x, vec.y, value);
+            return vec;
+        }
     }
 }
--- centauri-tac/Assets/Scripts/game/views/CardsView.cs
+++ centauri-tac/Assets/Scripts/game/views/CardsView.cs
@@ -10,8 +10,9 @@ namespace ctac {
 
         private CardModel selectedCard { get; set; }
 
-        private Vector3 baseCardOffset;
-        private Vector3 cardPositionOffset;
+        private Vector3 baseCardOffset = new Vector3(0, -54f, 0);
+        private Vector3 cardPositionOffset = new Vector3(60, 0, -1);
+        private const float maxCardHeight = 20f;
         private Vector3 dest;
 
         private Camera cardCamera;
@@ -19,9 +20,6 @@ namespace ctac {
 
         protected override void Start()
         {
-            baseCardOffset = new Vector3(0, -54f, 0);
-            cardPositionOffset = new Vector3(60, 0, -1);
-
             cardCamera = Camera.allCameras.FirstOrDefault(x => x.name == "CardCamera");
             CanvasRect = GameObject.Find("cardCanvas").GetComponent<RectTransform>();
         }
@@ -42,23 +40,29 @@ namespace ctac {
                 {
                     if (card == selectedCard)
                     {
-                        var mouseViewport = cardCamera.ScreenToViewportPoint(CrossPlatformInputManager.mousePosition);
-
-                        //calculate the position of the UI element
-                        //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
-
-                        dest.Set(
-                            ((mouseViewport.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-                            ((mouseViewport.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)),
-                            selectedCard.gameObject.transform.localPosition.z
-                        );
-
-                        card.gameObject.transform.localPosition = dest;
-                        continue;
+                        var dragPos = MouseToWorld( selectedCard.gameObject.transform.localPosition.z );
+                        if (dragPos.y < maxCardHeight)
+                        {
+                            card.gameObject.transform.localPosition = dragPos;
+                            continue;
+                        }
                     }
                 }
                 rectTransform.anchoredPosition3D = iTween.Vector3Update(rectTransform.anchoredPosition3D, dest, 10.0f);
             }
+        }
+
+        private Vector3 MouseToWorld(float z)
+        {
+            var mouseViewport = cardCamera.ScreenToViewportPoint(CrossPlatformInputManager.mousePosition);
+
+            //calculate the position of the UI element
+            //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+            return new Vector3(
+                ((mouseViewport.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+                ((mouseViewport.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)),
+                z
+            );
         }
 
         internal void onCardSelected(CardModel card)
