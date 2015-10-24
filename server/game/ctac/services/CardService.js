9a660fb0443249c0c3ff05effb4ce225b8a03f59
Dillon Shook <dshook@alumni.nmt.edu>
2015-10-24 13:12:37 -0400

    Register the card directory

--- server/game/ctac/services/CardService.js
+++ server/game/ctac/services/CardService.js
@@ -1,5 +1,7 @@
 import loglevel from 'loglevel-decorator';
 import ActivateCardProcessor from '../processors/ActivateCardProcessor.js';
+import requireDir from 'require-dir';
+
 
 /**
  * Expose the turn model and add the processor to the action pipeline
@@ -9,6 +11,16 @@ export default class CardService
 {
   constructor(app, queue)
   {
+    var cardRequires = requireDir('../../../../cards');
+    var cardDirectory = {};
+
+    for(let cardFileName in cardRequires){
+      let card = cardRequires[cardFileName];
+      cardDirectory[card.id] = card;
+    }
+    this.log.info('Registered cards %j', cardDirectory);
+    app.registerInstance('cardDirectory', cardDirectory);
+
     queue.addProcessor(ActivateCardProcessor);
   }
 }
