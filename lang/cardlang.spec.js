1bd86f099aabaa5b8e25ae290153352a7e0fdc5f
Dillon Shook <dshook@alumni.nmt.edu>
2015-11-29 16:03:22 -0500

    More cardlang tests

--- lang/cardlang.spec.js
+++ lang/cardlang.spec.js
@@ -27,5 +27,88 @@ test('basic play event', t => {
   ];
 
   t.deepEqual(d, expected);
+});
 
+test('Two actions on event', t => {
+  t.plan(1);
+
+  let parser = lang.parser;
+  let input = `
+  play{ 
+    DrawCard(PLAYER); 
+    SetAttribute(TARGET, current_health, 3);
+  }
+  `;
+
+  let d = parser.parse(input);
+
+  let expected = [
+    {
+      event: 'play',
+      actions: [
+        {
+          action: 'DrawCard',
+          args: [
+            'PLAYER'
+          ]
+        },
+        {
+          action: 'SetAttribute',
+          args: [
+            'TARGET',
+            'current_health',
+            '3'
+          ]
+        }
+      ]
+    }
+  ];
+
+  t.deepEqual(d, expected);
+});
+
+test('Two Events', t => {
+  t.plan(1);
+
+  let parser = lang.parser;
+  let input = `
+  play{ 
+    SetAttribute(TARGET, current_health, 3);
+  }
+  death{
+    DrawCard(PLAYER, 3);
+  }
+  `;
+
+  let d = parser.parse(input);
+
+  let expected = [
+    {
+      event: 'play',
+      actions: [
+        {
+          action: 'SetAttribute',
+          args: [
+            'TARGET',
+            'current_health',
+            '3'
+          ]
+        }
+      ]
+    },
+    {
+    event: 'death',
+    actions: [
+        {
+          action: 'DrawCard',
+          args: [
+            'PLAYER',
+            '3'
+          ]
+        }
+      ]
+    }
+  ];
+
+  t.deepEqual(d, expected);
 });
