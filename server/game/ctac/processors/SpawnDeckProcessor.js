9100b2cb98a3d3422685ce3e5c51e10f7c1f8f34
Dillon Shook <dshook@alumni.nmt.edu>
2016-02-09 21:05:39 -0500

    Starting to test targetable spells, client work needed

--- server/game/ctac/processors/SpawnDeckProcessor.js
+++ server/game/ctac/processors/SpawnDeckProcessor.js
@@ -32,7 +32,7 @@ export default class SpawnDeckProcessor
     let deck = this.cardState.decks[playerId];
 
     //dev hack, set one card you're working on to be half your deck
-    let testingCards = [21];
+    let testingCards = [21, 22];
 
     for(let c = 0; c < deckCards; c++){
       let randCardId = _.sample(cardIds);
