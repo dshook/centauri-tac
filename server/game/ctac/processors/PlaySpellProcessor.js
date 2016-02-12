0fe0fd6c17557d087d17fb96dc25a02ebdf69b68
Dillon Shook <dshook@alumni.nmt.edu>
2016-02-11 20:13:47 -0500

    Cancelling play spells without valid targets now

--- server/game/ctac/processors/PlaySpellProcessor.js
+++ server/game/ctac/processors/PlaySpellProcessor.js
@@ -29,19 +29,25 @@ export default class PlaySpellProcessor
 
     let cardPlayed = this.cardDirectory.directory[action.cardTemplateId];
 
-    this.cardEvaluator.evaluateSpellEvent('playSpell', cardPlayed, action.playerId, action.targetPieceId);
 
-    if(action.cardInstanceId !== null){
-      const playedCard = this.cardState.playCard(action.playerId, action.cardInstanceId);
-      if(!playedCard){
-        this.log.error('Card id %s was not found in player %s\'s hand', action.cardInstanceId, action.playerId);
-        queue.cancel(action, true);
+    if(this.cardEvaluator.evaluateSpellEvent('playSpell', cardPlayed, action.playerId, action.targetPieceId)){
+      if(action.cardInstanceId !== null){
+        const playedCard = this.cardState.playCard(action.playerId, action.cardInstanceId);
+        if(!playedCard){
+          this.log.error('Card id %s was not found in player %s\'s hand', action.cardInstanceId, action.playerId);
+          queue.cancel(action, true);
+        }
       }
-    }
 
-    queue.complete(action);
-    queue.push(new SetPlayerResource(action.playerId, -cardPlayed.cost));
-    this.log.info('played spell %s for player %s at %s',
-      cardPlayed.name, action.playerId, action.position);
+      queue.complete(action);
+      queue.push(new SetPlayerResource(action.playerId, -cardPlayed.cost));
+      this.log.info('played spell %s for player %s at %s',
+        cardPlayed.name, action.playerId, action.position);
+    }else{
+      //be sure to emit the cancel event so the client can respond
+      queue.cancel(action, true);
+      this.log.info('Play spell %s for player %s SCRUBBED',
+        cardPlayed.name, action.playerId);
+    }
   }
 }
