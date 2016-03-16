a1f12bc15e0ee4bca98f52b450e1f4b8b9bbb96b
Dillon Shook <dshook@alumni.nmt.edu>
2016-03-15 20:47:05 -0400

    Statuses in client

---
+++ centauri-tac/Assets/Scripts/game/models/Statuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ctac
+{
+    [Flags]
+    public enum Statuses
+    {
+      Silence = 1,
+      Shield = 2,
+      Paralyze = 4,
+      Taunt = 8,
+      Cloak = 16,
+      TechResist = 32,
+      Rooted = 64
+    }
+}
---
+++ centauri-tac/Assets/Scripts/game/models/server/PieceStatusChangeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ctac
+{
+    public class PieceStatusChangeModel
+    {
+        public int id { get; set; }
+        public int pieceId { get; set; }
+        public Statuses add { get; set; }
+        public Statuses remove { get; set; }
+
+        public Statuses statuses { get; set; }
+    }
+}
