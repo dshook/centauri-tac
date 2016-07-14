0967e5f903245c6e31325eef910b816d1bf7c2c5
Dillon Shook <dshook@alumni.nmt.edu>
2016-07-13 22:27:39 -0400

    Basin map

--- tasks/generate-map.js
+++ tasks/generate-map.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var outputFile = './maps/cubeland.json';
 
-var mapXSize = 10;
-var mapZSize = 10;
+var mapXSize = 11;
+var mapZSize = 11;
 var tileSet = ['grass', 'rock', 'clay'];
+var center = {x: Math.floor(mapXSize / 2), z: Math.floor(mapZSize / 2)};
 
 function random(min, max) {
   if (max == null) {
@@ -21,13 +22,27 @@ function sample(array){
   return array[random(array.length - 1)];
 }
 
+function tileDistance(posA, posB){
+  return Math.abs(posA.x - posB.x) + Math.abs(posA.z - posB.z);
+}
+
 var tiles = [];
 
 for(var x = 0; x < mapXSize; x++){
   for(var z = 0; z < mapZSize; z++){
+    var position = {x, z};
+    var distFromCenter = tileDistance(position, center);
+    position.y = (distFromCenter * 0.1).toFixed(3);
+    var material = 'grass';
+    if(distFromCenter > 2){
+      material = 'clay';
+    }
+    if(distFromCenter > 5){
+      material = 'rock';
+    }
     tiles.push({
-      transform: {x, y: randomReal(0, 0.9), z},
-      material: sample(tileSet)
+      transform: position,
+      material: material
     });
   }
 }
