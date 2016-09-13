a7947f7a1d606c3ea06a14ae582ad065b4bed363
Dillon Shook <dshook@alumni.nmt.edu>
2016-09-12 22:17:44 -0400

    Start of private actions & messages

--- server/game/ctac/actions/Message.js
+++ server/game/ctac/actions/Message.js
@@ -3,9 +3,11 @@ import BaseAction from './BaseAction.js';
 //Sends a string message to the client to display
 export default class Message extends BaseAction
 {
-  constructor(msg)
+  constructor(msg, playerId)
   {
     super();
+    this.private = true;
     this.message = msg;
+    this.playerId = playerId;
   }
 }
--- server/game/ctac/controllers/GameController.js
+++ server/game/ctac/controllers/GameController.js
@@ -242,6 +242,7 @@ export default class GameController
   _sendAction(player, action, cancelled = false)
   {
     if(action.serverOnly) return;
+    if(action.private && action.playerId && action.playerId !== player.id) return;
 
     const verb = cancelled ? 'actionCancelled:' : 'action:';
     player.client.send(verb + action.constructor.name, action);
