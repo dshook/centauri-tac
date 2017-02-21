2b9b81dc94692422794a44a60d90d56cef9dda8b
Dillon Shook <dshook@alumni.nmt.edu>
2017-02-20 19:49:37 -0500

    Fix server tests

--- server/tests/Processors.spec.js
+++ server/tests/Processors.spec.js
@@ -1,11 +1,13 @@
 import Application from 'billy';
 import ActionQueue from 'action-queue';
 import Player from 'models/Player';
+import Game from 'models/Game';
 
 import GameDataService from '../game/ctac/services/GameDataService.js';
 import ProcessorsService from '../game/ctac/services/ProcessorsService.js';
 import CardService from '../game/ctac/services/CardService.js';
 import MapService from '../game/ctac/services/MapService.js';
+import GameEventService from '../game/ctac/services/GameEventService.js';
 import ProcessorServiceTests from './ProcessorServiceTests.spec.js';
 
 
@@ -15,6 +17,10 @@ var app = new Application();
 const queue = new ActionQueue(T => app.make(T));
 app.registerInstance('queue', queue);
 
+var game = new Game();
+game.turnLengthMs = 10;
+app.registerInstance('game', game);
+
 let players = [];
 players.push(Player.fromTest(1));
 players.push(Player.fromTest(2));
@@ -22,6 +28,7 @@ app.registerInstance('players', players);
 app.service(MapService);
 app.service(GameDataService);
 app.service(CardService);
+app.service(GameEventService);
 app.service(ProcessorsService);
 
 app.service(ProcessorServiceTests);
