3d32ceae22da5c33e72219f2e5de86bebeb4dc4e
Dillon Shook <dshook@alumni.nmt.edu>
2017-05-30 22:02:28 -0400

    Added a bunch of bot models to classic set tests.  Little editor script to help out setting them up as well

---
+++ centauri-tac/Assets/Scripts/Editor/FixBot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+class FixBot : EditorWindow
+{
+    [MenuItem("Window/FixBot")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(FixBot));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GameObject thisObject = Selection.activeObject as GameObject;
+        if (thisObject == null)
+        {
+            return;
+        }
+
+        if (GUILayout.Button("Fix"))
+        {
+            var prefab = thisObject.transform.Find("c-bot_c_Prefab");
+            if (prefab == null)
+            {
+                Debug.LogWarning("Couldn't find prefab");
+                return;
+            }
+            thisObject.tag = "Piece";
+
+            for (var c = 0; c < prefab.childCount; c++)
+            {
+                var child = prefab.GetChild(c);
+                if (child.name == "Root_Bone") continue;
+                
+                var existingCollider = child.gameObject.GetComponent<MeshCollider>();
+                if (existingCollider != null) continue;
+
+                var mesh = child.gameObject.GetComponent<SkinnedMeshRenderer>();
+                var collider = child.gameObject.AddComponent<MeshCollider>();
+                collider.convex = true;
+                collider.sharedMesh = mesh.sharedMesh;
+            }
+        }
+
+    }
+
+}
