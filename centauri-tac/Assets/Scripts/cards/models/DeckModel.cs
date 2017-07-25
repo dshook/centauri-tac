e1833652a5ae6e732e7240449aee3e503396f8ef
Dillon Shook <dshook@alumni.nmt.edu>
2017-07-24 20:52:46 -0400

    Cards menu layout progress

---
+++ centauri-tac/Assets/Scripts/cards/models/DeckModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ctac
+{
+    //This deck model is for the card manager, not the in game deck
+    public class DeckModel
+    {
+        public string name { get; set; }
+        public List<CardModel> Cards = new List<CardModel>();
+    }
+}
---
+++ centauri-tac/Assets/Scripts/cards/views/DeckListView.cs
@@ -0,0 +1,64 @@
+﻿using strange.extensions.mediation.impl;
+using SVGImporter;
+using TMPro;
+using UnityEngine;
+
+namespace ctac {
+    public class DeckListView : View
+    {
+        public DeckModel deck { get; set; }
+
+        public RectTransform rectTransform { get; set; }
+
+        public GameObject bgGO;
+        public GameObject nameGO;
+
+        public TextMeshProUGUI nameText;
+
+        public SVGImage bgImage;
+
+        protected override void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        protected override void Start()
+        {
+            bgGO = transform.FindChild("Bg").gameObject;
+            nameGO = transform.FindChild("Name").gameObject;
+
+            bgImage = bgGO.GetComponent<SVGImage>();
+            nameText = nameGO.GetComponent<TextMeshProUGUI>();
+        
+            UpdateText();
+        }
+
+        void Update()
+        {
+        }
+
+        public void UpdateText()
+        {
+            //ResetTextColors();
+
+            nameText.text = deck.name;
+
+            //switch (card.rarity)
+            //{
+            //    case Rarities.Common:
+            //        bgImage.color = ColorExtensions.DesaturateColor(Colors.RarityCommon, 0.2f);
+            //        break;
+            //    case Rarities.Rare:
+            //        bgImage.color = ColorExtensions.DesaturateColor(Colors.RarityRare, 0.2f);
+            //        break;
+            //    case Rarities.Exotic:
+            //        bgImage.color = ColorExtensions.DesaturateColor(Colors.RarityExotic, 0.2f);
+            //        break;
+            //    case Rarities.Mythical:
+            //        bgImage.color = ColorExtensions.DesaturateColor(Colors.RarityMythical, 0.3f);
+            //        break;
+            //}
+
+        }
+    }
+}
