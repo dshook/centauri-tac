1f849364e091d6eb33fd1e87670dad35cac6a4eb
Dillon Shook <dshook@alumni.nmt.edu>
2017-07-31 22:16:13 -0400

    Automated client build script

--- Makefile
+++ Makefile
@@ -25,3 +25,13 @@ up-prod: build down-prod
 
 logs-prod:
 	docker-compose -p stac -f ./docker/docker-compose-prod.yml logs -f --tail 1000
+
+
+build-client-win:
+	"C:\\Program Files\\Unity\\Editor\\Unity.exe" -batchmode -quit -executeMethod BuildScript.WinBuild -projectPath "C:\\Users\\dillo_000.DILLON\\Documents\\Programming\\centauri-tac\\centauri-tac"
+
+build-client-win-dev:
+	"C:\\Program Files\\Unity\\Editor\\Unity.exe" -batchmode -quit -executeMethod BuildScript.WinDevBuild -projectPath "C:\\Users\\dillo_000.DILLON\\Documents\\Programming\\centauri-tac\\centauri-tac"
+
+build-client-all:
+	"C:\\Program Files\\Unity\\Editor\\Unity.exe" -batchmode -quit -executeMethod BuildScript.BuildAll -projectPath "C:\\Users\\dillo_000.DILLON\\Documents\\Programming\\centauri-tac\\centauri-tac"
---
+++ centauri-tac/Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using UnityEditor;
+
+class BuildScript
+{
+    private static string[] EnabledLevels()
+    {
+        return EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path).ToArray();
+    }
+
+    private static string basePath = "../build/";
+    private static string programName = "SolariaTactics.exe";
+
+    private static string buildPath(string build)
+    {
+        return string.Format("{0}{1}/", basePath, build);
+    }
+
+    public static void WinBuild()
+    {
+        Build("win", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    public static void WinDevBuild()
+    {
+        Build("dev", BuildTarget.StandaloneWindows64, BuildOptions.Development);
+    }
+
+    public static void MacBuild()
+    {
+        Build("osx", BuildTarget.StandaloneOSXIntel64, BuildOptions.None);
+    }
+
+    public static void MacDevBuild()
+    {
+        Build("osx-dev", BuildTarget.StandaloneOSXIntel64, BuildOptions.Development);
+    }
+
+    public static void BuildAll()
+    {
+        WinBuild();
+        WinDevBuild();
+        MacBuild();
+        MacDevBuild();
+    }
+
+    private static void Build(string build, BuildTarget target, BuildOptions options)
+    {
+        var path = buildPath(build);
+        FileUtil.DeleteFileOrDirectory(path);
+        BuildPipeline.BuildPlayer(EnabledLevels(), path + programName, target, options);
+        CopyAssets(path);
+    }
+
+    private static void CopyAssets(string path)
+    {
+        FileUtil.CopyFileOrDirectory("../cards", path + "cards");
+        FileUtil.CopyFileOrDirectory("../maps", path + "maps");
+    }
+
+}
