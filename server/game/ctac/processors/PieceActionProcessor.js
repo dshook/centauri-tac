a50c743bc6d238ec238498fb744b14a3b7375ff0
Dillon Shook <dshook@alumni.nmt.edu>
2017-08-08 22:18:29 -0400

    Some error handling in piece action processor but some other refactoring needs to be done

--- server/game/ctac/processors/PieceActionProcessor.js
+++ server/game/ctac/processors/PieceActionProcessor.js
@@ -1,5 +1,7 @@
 import PieceAction from '../actions/PieceAction.js';
+import Message from '../actions/Message.js';
 import loglevel from 'loglevel-decorator';
+import EvalError from '../cardlang/EvalError.js';
 
 /**
  * Run the selector and create an action for each selected piece
@@ -20,23 +22,35 @@ export default class PieceActionProcessor
     }
 
     let additionalActions = [];
-    if(action.selector && action.pieceSelectorParams){
-
-      let selected = this.selector.selectPieces(action.selector, action.pieceSelectorParams);
-      this.log.info('Selected %s pieces for action %s', selected.length, action.actionClass.name);
-      this.pieceState.lastSelectedPieces = selected.map(p => p.id) || [];
-      if(selected && selected.length > 0){
-        for(let s of selected){
-          let actionParams = Object.assign({}, action.actionParams, {pieceId: s.id});
-          let newAction = new action.actionClass(actionParams);
-          newAction.activatingPieceId = action.activatingPieceId;
-
-          additionalActions.push(newAction);
-        }
-      }
-    }else{
+    if(!action.selector || !action.pieceSelectorParams){
       this.log.error('Cannot expand piece action without selector or selector params')
       return queue.cancel(action);
+    }
+
+    let selected = null;
+    try{
+      selected = this.selector.selectPieces(action.selector, action.pieceSelectorParams);
+    }catch(e){
+      if(e instanceof EvalError){
+        this.log.info('EvalError in piece action %s', e.message);
+        if(action.pieceSelectorParams.controllingPlayerId){
+          queue.push(new Message(e.message, action.pieceSelectorParams.controllingPlayerId));
+        }
+        return queue.cancel(action, true);
+      }
+      this.log.error('Error in piece action processor', e);
+      throw e;
+    }
+    this.log.info('Selected %s pieces for action %s', selected.length, action.actionClass.name);
+    this.pieceState.lastSelectedPieces = selected.map(p => p.id) || [];
+    if(selected && selected.length > 0){
+      for(let s of selected){
+        let actionParams = Object.assign({}, action.actionParams, {pieceId: s.id});
+        let newAction = new action.actionClass(actionParams);
+        newAction.activatingPieceId = action.activatingPieceId;
+
+        additionalActions.push(newAction);
+      }
     }
 
     queue.complete(action);
