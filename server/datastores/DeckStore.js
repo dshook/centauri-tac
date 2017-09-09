22053d3a4f1979aca77e973fa1c8e49d91870350
Dillon Shook <dshook@alumni.nmt.edu>
2017-09-08 21:40:47 -0400

    Stubbing out some card saving and deleting methods

--- server/datastores/DeckStore.js
+++ server/datastores/DeckStore.js
@@ -56,4 +56,58 @@ export default class DeckStore
     return resp.toArray();
   }
 
+  //Update or insert a deck
+  //TODO: might need a transaction at some point?
+  async upsertDeck(deck){
+    let sql = `
+      insert into player_decks (id, player_id, name, race, type, is_valid)
+        values (@id, @playerId, @name, @race, @type, @isValid)
+      on conflict (id)
+      do update set (name, race, type, is_valid) = (@name, @race, @type, @isValid)
+      where id = @id
+      returning id;
+    `;
+
+    let id = await this.sql.query(sql, deck);
+
+    await this.sql.query('delete from deck_cards where deck_id = @id', {id});
+
+    await Promise.map(deck.cards, card => this.sql.query(`
+        INSERT INTO deck_cards (deck_id, card_template_id, quantity)
+        VALUES (@deckId, @cardTemplateId, @quantity);
+      `, card)
+    );
+
+    deck.id = id;
+
+    return deck;
+  }
+
+  async deleteDeck(deckId, playerId){
+    let sql = `
+      select *
+      from player_decks
+      where id = @deckId and player_id = @playerId
+    `;
+
+    const resp = await this.sql.tquery(PlayerDeck)(sql, {deckId, playerId});
+
+    if(!resp){
+      this.log.warn('No deck found to delete with id %s player %s', deckId, playerId)
+      return false;
+    }
+
+    await this.sql.query(`
+      delete from deck_cards
+      where deck_id = @deckId
+    ` , {deckId});
+
+    await this.sql.query(`
+      delete from player_decks
+      where id = @deckId and player_id = @playerId
+    ` , {deckId, playerId});
+
+    return true;
+  }
+
 }
