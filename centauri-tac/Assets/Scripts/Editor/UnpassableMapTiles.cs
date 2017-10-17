cd32f25cdc9cddaf659fb87e9c55dc14a76a076c
Dillon Shook <dshook@alumni.nmt.edu>
2017-10-16 21:35:04 -0400

    Fixed unpassable tile issue with mars map, and made tool to find unpassable tiles easily

---
+++ centauri-tac/Assets/Scripts/Editor/UnpassableMapTiles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using ctac;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+class UnpassableMapTiles : EditorWindow
+{
+    [MenuItem("Window/UnpassableMapTiles")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(UnpassableMapTiles));
+        window.Show();
+    }
+
+    string levelName;
+    void OnGUI()
+    {
+        var mapGO = GameObject.Find("Map");
+        var tileGO = mapGO.transform.FindChild("Tiles");
+
+        var selected = new List<GameObject>();
+        for (int t = 0; t < tileGO.childCount; t++)
+        {
+            var tile = tileGO.transform.GetChild(t);
+
+            var tileView = tile.GetComponent<TileView>();
+            if (tileView == null) continue;
+
+            if (tileView.unpassable)
+            {
+                selected.Add(tile.gameObject);
+            }
+        }
+
+        Selection.objects = selected.ToArray();
+    }
+
+}
