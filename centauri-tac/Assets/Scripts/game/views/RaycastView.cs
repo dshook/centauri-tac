1c2f029ed61f75bcddfe58ec5d0424e264b77bbd
Dillon Shook <dshook@alumni.nmt.edu>
2017-11-19 15:04:30 -0500

    Ignore raycasts when mouse is over UI

--- centauri-tac/Assets/Scripts/game/views/RaycastView.cs
+++ centauri-tac/Assets/Scripts/game/views/RaycastView.cs
@@ -1,6 +1,8 @@
 using strange.extensions.mediation.impl;
+using System;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityStandardAssets.CrossPlatformInput;
 
 namespace ctac
@@ -10,34 +12,43 @@ namespace ctac
         [Inject] public RaycastModel model { get; set; }
         [Inject] public MapModel map { get; set; } 
         private Camera cardCamera;
+        EventSystem eventSystem;
 
         int cardCanvasLayer = -1;
-        int tileMask = -1;
+        int tileLayer = -1;
         new void Awake()
         {
             cardCamera = Camera.allCameras.FirstOrDefault(x => x.name == Constants.cardCamera);
             cardCanvasLayer = LayerMask.GetMask(Constants.cardCanvas);
-            tileMask = LayerMask.GetMask("Tile");
+            tileLayer = LayerMask.GetMask("Tile");
+            eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
         }
 
         void Update()
         {
+            //Reset everything
+            model.tile = null;
+            model.piece = null;
+            model.worldHit = null;
+            model.cardCanvasHit = null;
 
-            //camera movement
+            //Ignore any raycasts if we're over a UI object
+            if (eventSystem.IsPointerOverGameObject())
+            {
+                return;
+            }
+
             //test click position to see if we hit the ground
             Ray camRay = Camera.main.ScreenPointToRay(CrossPlatformInputManager.mousePosition);
 
             RaycastHit tileHit;
-            if (Physics.Raycast(camRay, out tileHit, Constants.cameraRaycastDist, tileMask))
+            if (Physics.Raycast(camRay, out tileHit, Constants.cameraRaycastDist, tileLayer))
             {
                 model.tile = map.tiles.Get(tileHit.collider.gameObject.transform.position.ToTileCoordinates());
             }
-            else
-            {
-                model.tile = null;
-            }
 
             RaycastHit objectHit;
+            //world hit for everything else, but also ignore UI layer hits
             if (Physics.Raycast(camRay, out objectHit, Constants.cameraRaycastDist))
             {
                 //walk up 3 levels at most to try to see if we're in a piece
@@ -51,15 +62,7 @@ namespace ctac
                 {
                     model.piece = objectHit.collider.gameObject.GetComponentInParent<PieceView>();
                 }
-                else
-                {
-                    model.piece = null;
-                }
                 model.worldHit = objectHit;
-            }
-            else
-            {
-                model.worldHit = null;
             }
 
             //card interaction selection
@@ -71,10 +74,7 @@ namespace ctac
             {
                 model.cardCanvasHit = cardHit;
             }
-            else
-            {
-                model.cardCanvasHit = null;
-            }
+
 
         }
     }
